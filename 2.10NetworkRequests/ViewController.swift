a9a10b83fec1dc2a80aaca73fbed4e2e13bd3404
KaterinaKugeneva <kugeneva1@gmail.com>
2021-11-26 09:11:38 +0300

    Initial Commit

---
+++ 2.10NetworkRequests/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.10NetworkRequests
+//
+//  Created by Ekaterina Kugeneva on 26.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
